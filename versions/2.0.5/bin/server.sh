8cd70e0b657e5f49dc6c98cab1477b12504bdaf3
Kulysoft1 <Kulysoft1@users.noreply.github.com>
2015-03-25 00:12:00 +0200

    Update server.sh

--- versions/2.0.5/bin/server.sh
+++ versions/2.0.5/bin/server.sh
@@ -44,14 +44,14 @@ done
 # Get standard environment variables
 PRGDIR=`dirname "$PRG"`
 
-# Only set ORIENTDB2_HOME if not already set
-[ -f "$ORIENTDB2_HOME"/bin/server.sh ] || ORIENTDB2_HOME=`cd "$PRGDIR/.." ; pwd`
-export ORIENTDB2_HOME
-cd "$ORIENTDB2_HOME/bin"
+# Only set ORIENTDB_HOME if not already set
+[ -f "$ORIENTDB_HOME"/bin/server.sh ] || ORIENTDB_HOME=`cd "$PRGDIR/.." ; pwd`
+export ORIENTDB_HOME
+cd "$ORIENTDB_HOME/bin"
 
 if [ ! -f "${CONFIG_FILE}" ]
 then
-  CONFIG_FILE=$ORIENTDB2_HOME/config/orientdb-server-config.xml
+  CONFIG_FILE=$ORIENTDB_HOME/config/orientdb-server-config.xml
 fi
 
 # Raspberry Pi check (Java VM does not run with -server argument on ARMv6)
@@ -68,9 +68,9 @@ else
 fi
 export JAVA
 
-LOG_FILE=$ORIENTDB2_HOME/config/orientdb-server-log.properties
-WWW_PATH=$ORIENTDB2_HOME/www
-ORIENTDB2_SETTINGS="-Dprofiler.enabled=true"
+LOG_FILE=$ORIENTDB_HOME/config/orientdb-server-log.properties
+WWW_PATH=$ORIENTDB_HOME/www
+ORIENTDB_SETTINGS="-Dprofiler.enabled=true"
 JAVA_OPTS_SCRIPT="-Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9"
 
 # ORIENTDB MAXIMUM HEAP. USE SYNTAX -Xmx<memory>, WHERE <memory> HAS THE TOTAL MEMORY AND SIZE UNIT. EXAMPLE: -Xmx512m
@@ -78,4 +78,4 @@ MAXHEAP=-Xmx512m
 # ORIENTDB MAXIMUM DISKCACHE IN MB, EXAMPLE, ENTER -Dstorage.diskCache.bufferSize=8192 FOR 8GB
 MAXDISKCACHE=""
 
-exec "$JAVA" $JAVA_OPTS $MAXHEAP $JAVA_OPTS_SCRIPT $ORIENTDB2_SETTINGS $MAXDISKCACHE -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dorientdb.build.number="UNKNOWN@r${buildNumber}; 2015-03-12 22:59:10+0000" -cp "$ORIENTDB2_HOME/lib/orientdb-server-2.0.5.jar:$ORIENTDB2_HOME/lib/*" $* com.orientechnologies.orient.server.OServerMain
+exec "$JAVA" $JAVA_OPTS $MAXHEAP $JAVA_OPTS_SCRIPT $ORIENTDB_SETTINGS $MAXDISKCACHE -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dorientdb.build.number="UNKNOWN@r${buildNumber}; 2015-03-12 22:59:10+0000" -cp "$ORIENTDB_HOME/lib/orientdb-server-2.0.5.jar:$ORIENTDB_HOME/lib/*" $* com.orientechnologies.orient.server.OServerMain
