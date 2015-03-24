0c5dfcb84bc829ac023a58557db4746adbb24025
Kulysoft1 <Kulysoft1@users.noreply.github.com>
2015-03-24 21:39:23 +0200

    Update console.sh

--- versions/2.0.5/bin/console.sh
+++ versions/2.0.5/bin/console.sh
@@ -40,4 +40,4 @@ TRUSTSTORE=$ORIENTDB2_HOME/config/cert/orientdb-console.ts
 TRUSTSTORE_PASS=password
 SSL_OPTS="-Xmx512m -Dclient.ssl.enabled=false -Djavax.net.ssl.keyStore=$KEYSTORE -Djavax.net.ssl.keyStorePassword=$KEYSTORE_PASS -Djavax.net.ssl.trustStore=$TRUSTSTORE -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASS"
 
-"$JAVA" -client $JAVA_OPTS $ORIENTDB2_SETTINGS $SSL_OPTS -Dfile.encoding=utf-8 -Dorientdb.build.number="UNKNOWN@r${buildNumber}; 2015-03-12 22:59:10+0000" -cp "$ORIENTDB2_HOME/lib/orientdb-tools-2.0.5.jar:$ORIENTDB2_HOME/lib/*:$ORIENTDB_HOME/plugins/*" com.orientechnologies.orient.graph.console.OGremlinConsole $*
+"$JAVA" -client $JAVA_OPTS $ORIENTDB2_SETTINGS $SSL_OPTS -Dfile.encoding=utf-8 -Dorientdb.build.number="UNKNOWN@r${buildNumber}; 2015-03-12 22:59:10+0000" -cp "$ORIENTDB2_HOME/lib/orientdb-tools-2.0.5.jar:$ORIENTDB2_HOME/lib/*:$ORIENTDB2_HOME/plugins/*" com.orientechnologies.orient.graph.console.OGremlinConsole $*
